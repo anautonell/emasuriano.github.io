b17a548f667f1aa65780a079599735e3c9de2955
EmaSuriano <emanuel.suriano@gmail.com>
2018-01-26 00:53:01 -0300

    fixing gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,8 +60,7 @@ module.exports = {
                   url: site.siteMetadata.url + edge.node.fields.slug,
                   guid: site.siteMetadata.url + edge.node.fields.slug,
                   custom_elements: [{ 'content:encoded': edge.node.html }],
-                }),
-              ),
+                })),
             query: `
               {
                 allMarkdownRemark(
