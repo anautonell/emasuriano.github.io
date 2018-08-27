a461117263d3fbaa191a794f3aed696facf1dcd7
EmaSuriano <emanuel.suriano@gmail.com>
2018-08-27 23:08:25 +0200

    Add random palette on every refresh

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,6 +1,9 @@
-const colors = {
+const randomItemFromArray = array =>
+  array[Math.floor(Math.random() * array.length)];
+
+const pinkPalette = {
   background: '#FFFFFF',
-  backgroundDark: '#cecdfe',
+  backgroundDark: '#f0e6f6',
 
   primary: '#7c37ad',
   primaryLight: '#ae66df',
@@ -11,6 +14,31 @@ const colors = {
   secondaryDark: '#c60055',
 };
 
+const greenPalette = {
+  background: '#FFFFFF',
+  backgroundDark: '#e1f4fd',
+
+  primary: '#02a3ee',
+  primaryLight: '#65d4ff',
+  primaryDark: '#0075bb',
+
+  secondary: '#02eec3',
+  secondaryLight: '#6afff6',
+  secondaryDark: '#00ba93',
+};
+
+const coralPalette = {
+  background: '#FFFFFF',
+  backgroundDark: '#feebf0',
+
+  primary: '#e63946',
+  primaryLight: '#ff7071',
+  primaryDark: '#ad001f',
+  secondary: '#5ed1f3',
+  secondaryLight: '#97ffff',
+  secondaryDark: '#0ca0c0',
+};
+
 export default {
-  colors,
+  colors: randomItemFromArray([pinkPalette, greenPalette, coralPalette]),
 };
